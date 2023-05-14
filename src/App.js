346daf1e7e96d71453e5828c442114fb277a1683
Alex Vaschilov <vaschilovalex@gmail.com>
2023-05-15 01:16:12 +0300

    Add Person component to display names, add function addPerson and conrolle component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
-const App = () => {
+import { useState } from "react";
+
+const Person = ({ person }) => {
   return (
     <div>
-      Home page
+      {person.name}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" }
+  ]);
+  const [newName, setNewName] = useState("a new person...");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: 
+          <input 
+            value={newName} 
+            onChange={handlePersonChange} 
+          />
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+
+      {persons.map(person => 
+        <Person key={person.name} person={person} />
+      )}
     </div>
   );
 }
